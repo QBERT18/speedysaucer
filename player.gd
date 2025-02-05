7d6d607399a8358745b10c2cf3abf81862061aca
Valeh Mustafazade <valeh.mustafayev.98@gmail.com>
2025-02-05 20:16:37 +0100

    Following The Player With Camera2D

--- player.gd
+++ player.gd
@@ -1,7 +1,7 @@
 extends RigidBody2D
 
 func _physics_process(delta):
-	var force = 100
+	var force = 1000
 	if Input.is_action_pressed("move_up"):
 		apply_force(Vector2(0, -force))
 	if Input.is_action_pressed("move_right"):
