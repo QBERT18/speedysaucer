47ccc0b6dd949cc26d2266c72c35538fe80c5530
Valeh Mustafazade <valeh.mustafayev.98@gmail.com>
2025-02-05 19:49:04 +0100

    Processing & apply_force( )

--- player.gd
+++ player.gd
@@ -2,8 +2,12 @@ extends RigidBody2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	apply_impulse(Vector2(25.0, 0.0))
-
+	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
+	# print("Frames per second: " + str(Engine.get_frames_per_second()))
 	pass
+
+func _physics_process(delta):
+	apply_force(Vector2(25.0, 0.0))
+	
